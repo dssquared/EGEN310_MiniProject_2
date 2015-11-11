1014f794d7381e64c361164415766f30a0a01bbb
David <dpschwehr@gmail.com>
2015-11-11 10:44:54 -0700

    Initial commit, start of salinity probe code for EGEN310 MiniProject #2

---
+++ salinityProbe.ino
@@ -0,0 +1,29 @@
+#include <Statistics.h>
+#include <IntStatistics.h>
+/*
+*******************************************************************
+****         Arduino sketch for EGEN310 MiniProject #2         ****
+****      Salinity probe made of an anode and sensor to read   ****
+****  voltage through ADC to measure conductivity. Serial I/O  ****
+****  will be used to control sketch remotely and operate      ****
+****  servos for mixer and probe movement, 12v DC fluid pump   ****
+****  and all other code required for the EGEN310 assignment.  ****
+*******************************************************************
+by David Schwehr Novembr 2015 (for Group D5)
+   github: dssquared
+*/
+
+
+void setup()
+{
+
+  /* add setup code here */
+
+}
+
+void loop()
+{
+
+  /* add main program code here */
+
+}
